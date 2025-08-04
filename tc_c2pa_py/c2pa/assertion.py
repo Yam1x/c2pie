f7accf3ef02adbd728416d659d7c8660b82f7300
JohnDoe <contact@tourmalinecore.com>
2025-08-04 14:49:02 +0500

    Fix default value of hash data assertion For some reason only values between 256 and 65535 work (so, 3-4 ditig hex numbers). We're not sure why

--- tc_c2pa_py/c2pa/assertion.py
+++ tc_c2pa_py/c2pa/assertion.py
@@ -38,7 +38,7 @@ class HashDataAssertion(Assertion):
 
     def __init__(self, cai_offset, hashed_data, additional_exclusions):
         hash_data_schema = {
-            "exclusions": [{"start": cai_offset, "length": -1}] + additional_exclusions, # set length to 65535, library will calculate length by itself
+            "exclusions": [{"start": cai_offset, "length": 65535}] + additional_exclusions, # set length to 65535, library will calculate length by itself
             "name": "jumbf manifest",
             "alg": "sha256",
             "hash": hashed_data,
@@ -51,7 +51,7 @@ class HashDataAssertion(Assertion):
     def set_hash_data_length(self, length):
         if self.schema['name'] == 'jumbf manifest':
             for exclusion_id in range(len(self.schema['exclusions'])):
-                if self.schema['exclusions'][exclusion_id]['length'] == -1:
+                if self.schema['exclusions'][exclusion_id]['length'] == 65535:
                     self.schema['exclusions'][exclusion_id]['length'] = length
         
         self.payload = self.get_payload_from_schema()

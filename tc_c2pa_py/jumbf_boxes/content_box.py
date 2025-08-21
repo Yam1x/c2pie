9233893acaee52827ef141281d9e817f104acb64
SupremeSoviet <vanishedgrace@list.ru>
2025-08-21 19:41:40 +0000

    fix: reformat code with ruff

--- tc_c2pa_py/jumbf_boxes/content_box.py
+++ tc_c2pa_py/jumbf_boxes/content_box.py
@@ -4,5 +4,5 @@ from tc_c2pa_py.jumbf_boxes.box import Box
 
 
 class ContentBox(Box):
-    def __init__(self, box_type=b"json".hex(), payload=b""):
+    def __init__(self, box_type=b"json".hex(), payload=b""):  # noqa: B008
         super().__init__(box_type=box_type, payload=payload)

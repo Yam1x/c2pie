233f8df9383466ac6ea60c21a82e73c9083508eb
lasnown <125271962+lasnown@users.noreply.github.com>
2025-09-16 14:59:05 +0500

    feat(tests): add tests increasing test coverage

--- tests/c2pa/assertion_test.py
+++ tests/c2pa/assertion_test.py
@@ -1,4 +1,4 @@
-from c2pie.c2pa.assertion import Assertion
+from c2pie.c2pa.assertion import Assertion, HashDataAssertion
 from c2pie.utils.assertion_schemas import C2PA_AssertionTypes, cbor_to_bytes, json_to_bytes
 from c2pie.utils.content_types import jumbf_content_types
 
@@ -24,6 +24,19 @@ def test_create_assertion_with_true_type():
     test_assertion = Assertion(C2PA_AssertionTypes.creative_work, {})
 
     assert test_assertion.type == C2PA_AssertionTypes.creative_work
+
+
+def test_create_assertion_with_thumbnail_type():
+    test_assertion = Assertion(C2PA_AssertionTypes.thumbnail, {})
+
+    assert test_assertion.get_content_type() == jumbf_content_types["codestream"]
+
+
+def test_assertion_cannot_create_with_no_type():
+    test_assertion = Assertion(None, {})  # type: ignore
+
+    assert test_assertion.type not in C2PA_AssertionTypes
+    assert test_assertion.get_content_type() == b""
 
 
 def test_create_assertion_with_correct_schema():
@@ -77,3 +90,9 @@ def test_assertion_content_boxes_not_empty():  # noqa: F811
     test_assertion = Assertion(C2PA_AssertionTypes.creative_work, creative_work_schema)
 
     assert len(test_assertion.content_boxes) != 0
+
+
+def test_additional_extensions_adding_for_hash_data_assertions():
+    additional_exclusion = {"some_extension": 343}
+    test_assertion = HashDataAssertion(cai_offset=124, hashed_data=b"", additional_exclusions=[additional_exclusion])
+    assert additional_exclusion in test_assertion.schema["exclusions"]
